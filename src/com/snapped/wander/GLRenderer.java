7c7365ed28799d6e2eb1b39fc620375020a6b155
Josh Vanderwillik <joshwillik@gmail.com>
2013-11-13 22:55:37 -0500

    Add triangle class, get closer to rendering shapes

--- src/com/snapped/wander/GLRenderer.java
+++ src/com/snapped/wander/GLRenderer.java
@@ -4,33 +4,65 @@ import android.opengl.EGLConfig;
 import javax.microedition.khronos.opengles.*;
 import javax.microedition.khronos.egl.*;
 import android.content.Context;
+import java.nio.*;
+
 public class GLRenderer implements GLSurfaceView.Renderer
 {	
 	private GL gl;
+	private final String vertexShaderCode = 
+	"uniform mat4 uMVPMatrix; \n" 
+	+ "attribute vec4 vPosition; \n"
+	+"void main(){"
+	+	"gl_position = uMVPMatrix * vPosition; \n:"
+	+"}";
+	
 	public GLRenderer(Context context, GL gl){
 		this.gl = gl;
 		
 	}
 
-	public void onSurfaceCreated(GL10 p1, javax.microedition.khronos.egl.EGLConfig p2)
+	public void onSurfaceCreated(GL10 lgl, javax.microedition.khronos.egl.EGLConfig p2)
 	{
-		// TODO: Implement this method
-	}
-	
-
-	public void onSurfaceCreated(GL10 p1, EGLConfig p2)
-	{
-		// TODO: Implement this method
+		GLES20.glClearColor(1.0f, 0.5f, 0.5f, 0.8f);
+		lgl.glEnable(GL10.GL_CULL_FACE);
+		lgl.glCullFace(GL10.GL_BACK);
 	}
 
-	public void onSurfaceChanged(GL10 p1, int p2, int p3)
+	public void onSurfaceChanged(GL10 lgl, int w, int h)
 	{
-		// TODO: Implement this method
+		lgl.glViewport(0, 0, w, h);
+		float ratio = (float) w/h;
+		
+		lgl.glMatrixMode(GL10.GL_PROJECTION);
+		lgl.glLoadIdentity();
+		lgl.glFrustumf(-ratio, ratio, -1, 1, 1, 10);
 	}
 
 	public void onDrawFrame(GL10 p1)
 	{
 		GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
 		//game drawing logic here
+	}
+}
+
+class Triangle{
+	private FloatBuffer vertexBuffer;
+	static final int COORDS_PER_VERTEX = 3;
+	static float[] triangleCoords = {
+		0.0f, 0.5f, 0.0f
+		, 0.5f, -0.5f, 0.0f
+		, -0.5f, -0.5f, 0.0f
+	};
+
+	static float[] triangleColor = {
+		0.3f, 0.5f, 0.6f, 0.7f
+	};
+	
+	public Triangle(){
+		ByteBuffer bb = ByteBuffer.allocateDirect(triangleCoords.length * 4);
+		bb.order(ByteOrder.nativeOrder());
+		vertexBuffer = bb.asFloatBuffer();
+		vertexBuffer.put(triangleCoords);
+		vertexBuffer.position(0);		
 	}
 }
--- src/com/snapped/wander/MainActivity.java
+++ src/com/snapped/wander/MainActivity.java
@@ -3,15 +3,36 @@ package com.snapped.wander;
 import android.app.*;
 import android.os.*;
 import android.content.Intent;
+import android.view.*;
+import android.text.*;
 
 public class MainActivity extends Activity
 {
+	private GL gl;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
 	{
         super.onCreate(savedInstanceState);
-		GL gl = new GL(this);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(
+			WindowManager.LayoutParams.FLAG_FULLSCREEN
+			, WindowManager.LayoutParams.FLAG_FULLSCREEN
+			);
+		
+		gl = new GL(this);
 		setContentView(gl);
     }
+	
+	@Override
+	public void onPause(){
+		super.onResume();
+		gl.onPause();
+	}
+	
+	@Override
+	public void onResume(){
+		super.onResume();
+		gl.onResume();
+	}
 }
